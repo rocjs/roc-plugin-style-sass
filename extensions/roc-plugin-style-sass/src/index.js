9e6fb144fac82d74510ea72009ef7d6fe0d4e602
Gustaf Dalemar <gustaf.dalemar@gmail.com>
2016-11-21 13:21:07 +0100

    Use the new setting for toggling source maps

--- extensions/roc-plugin-style-sass/src/index.js
+++ extensions/roc-plugin-style-sass/src/index.js
@@ -5,7 +5,7 @@ const BOURBON_PATH = require('bourbon').includePaths;
 // eslint-disable-next-line
 export const roc = {
     required: {
-        'roc-plugin-style-css': '^1.0.0-beta',
+        'roc-plugin-style-css': '^1.0.0-beta.5',
     },
     config: {
         settings: {
@@ -28,15 +28,16 @@ export const roc = {
             },
         },
     },
+    /* eslint-disable no-nested-ternary, prefer-template */
     actions: [{
         extension: 'roc-plugin-style-css',
         hook: 'add-style',
         description: 'Adds Sass support to Webpack.',
         action: ({ context: { config: { settings } } }) => () => () => ({
             extensions: ['sass', 'scss'],
-            loaders: `${require.resolve('sass-loader')}?${settings.build.sass.useBourbon ?
-                `includePaths[]=${BOURBON_PATH}` : ''
-            }`,
+            loaders: `${require.resolve('sass-loader')}?` +
+                settings.build.style.sourceMap ? 'sourceMap&' : '' +
+                settings.build.sass.useBourbon ? `includePaths[]=${BOURBON_PATH}` : '',
         }),
     }],
 };
